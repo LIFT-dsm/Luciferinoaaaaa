b2cf29d0d86ec5dbf9e86dec459c0f9f085f5bfd
timdind <no3232164@dsm.hs.kr>
2024-09-24 19:40:57 +0900

    fix:: partyadd에 daytimepicker라이브러리 적용

--- pages/partyadd.tsx
+++ pages/partyadd.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   SafeAreaView,
   StyleSheet,
@@ -8,9 +8,55 @@ import {
   View,
   TouchableOpacity,
 } from "react-native";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+
+interface PartyTime {
+  startTime: number;
+  endTime: number;
+}
 
 function Partyadd(): React.JSX.Element {
   const isDarkMode = useColorScheme() === "dark";
+  const [partyTime, setPartyTime] = useState<PartyTime>({
+    startTime: 0,
+    endTime: 0,
+  });
+  const [isStartPickerVisible, setStartPickerVisibility] = useState(false);
+  const [isEndPickerVisible, setEndPickerVisibility] = useState(false);
+
+  const formatTime = (time: number) => {
+    const date = new Date(time);
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    return `${hours < 10 ? "0" : ""}${hours}:${
+      minutes < 10 ? "0" : ""
+    }${minutes}`;
+  };
+
+  const showStartPicker = () => {
+    setStartPickerVisibility(true);
+  };
+
+  const showEndPicker = () => {
+    setEndPickerVisibility(true);
+  };
+
+  const handleStartConfirm = (time: Date) => {
+    setPartyTime((prev) => ({ ...prev, startTime: time.getTime() }));
+    setStartPickerVisibility(false);
+  };
+
+  const handleEndConfirm = (time: Date) => {
+    setPartyTime((prev) => ({ ...prev, endTime: time.getTime() }));
+    setEndPickerVisibility(false);
+  };
+  const hideStartPicker = () => {
+    setStartPickerVisibility(false);
+  };
+
+  const hideEndPicker = () => {
+    setEndPickerVisibility(false);
+  };
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? "#000000" : "#FFFFFF",
@@ -39,18 +85,23 @@ function Partyadd(): React.JSX.Element {
         ></TextInput>
         <Text style={styles.inputText}>시간</Text>
         <View style={styles.timeContainer}>
-          <TextInput
-            placeholder="시작 시간"
-            style={styles.timesetStyle}
-            keyboardType="numeric"
-          ></TextInput>
+          <TouchableOpacity onPress={showStartPicker} style={styles.timeView}>
+            <Text>
+              {partyTime.startTime
+                ? formatTime(partyTime.startTime)
+                : "시작 시간"}
+            </Text>
+          </TouchableOpacity>
+
           <Text style={styles.timeMid}>~</Text>
-          <TextInput
-            placeholder="종료 시간"
-            style={styles.timesetStyle}
-            keyboardType="numeric"
-          ></TextInput>
+
+          <TouchableOpacity onPress={showEndPicker} style={styles.timeView}>
+            <Text>
+              {partyTime.endTime ? formatTime(partyTime.endTime) : "종료 시간"}
+            </Text>
+          </TouchableOpacity>
         </View>
+
         <Text style={styles.inputText}>지역</Text>
         <TextInput
           style={styles.inputStyle}
@@ -61,6 +112,22 @@ function Partyadd(): React.JSX.Element {
           <Text style={styles.buttonText}>파티 만들기</Text>
         </TouchableOpacity>
       </View>
+
+      <DateTimePickerModal
+        isVisible={isStartPickerVisible}
+        mode="time"
+        onConfirm={handleStartConfirm}
+        onCancel={hideStartPicker}
+        locale="ko_KR"
+      />
+
+      <DateTimePickerModal
+        isVisible={isEndPickerVisible}
+        mode="time"
+        onConfirm={handleEndConfirm}
+        onCancel={hideEndPicker}
+        locale="ko_KR"
+      />
     </SafeAreaView>
   );
 }
@@ -98,13 +165,15 @@ const styles = StyleSheet.create({
     paddingHorizontal: 15,
     backgroundColor: "white",
   },
-  timesetStyle: {
+  timeView: {
     width: "37%",
     height: 48,
     borderColor: "#000000",
     borderRadius: 8,
     borderWidth: 1,
     paddingHorizontal: 15,
+    justifyContent: "center",
+    alignItems: "center",
     backgroundColor: "white",
     textAlign: "center",
   },
